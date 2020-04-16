6fadb615b180e5ad992f4ac1ff0732b32b779e5d
SubhoBasak <subhobasak50@gmail.com>
2020-04-16 20:42:45 +0530

    added find and copy method

--- SubhoDataStructure/binary_tree.py
+++ SubhoDataStructure/binary_tree.py
@@ -10,6 +10,17 @@ class Tree:
         self.stack = []
 
     def add(self, val):
+        """
+        Add new value to the tree.
+
+        :param val: value may be integer, float or node
+        :return: if the value is added to the root then return 0,
+        if the value is added to the left side of the parent node then return 1,
+        if the value is added ot the right side of the parent node then return 2,
+        and if the value is already in the tree then is return -1
+        """
+        if type(val) == Node:
+            val = val.val
         if self.root == None:
             self.root = Node(val)
             return 0
@@ -79,16 +90,88 @@ class Tree:
             self.__post_order(cur_node.right_child)
         self.stack.append(cur_node)
 
+    def level_order(self):
+        if self.root != None:
+            self.stack = []
+            self.cur_ord = 'level_order'
+
     def show(self, order = 'pre_order'):
+        """
+        show all the values of the tree.
+        :param order: value may be "pre_order" or "in_order" or "post_order"
+        """
         if order == 'pre_order':
-            self.pre_order()
+            if self.cur_ord != order:
+                self.pre_order()
         elif order == 'in_order':
-            self.in_order()
+            if self.cur_ord != order:
+                self.in_order()
         elif order == 'post_order':
-            self.post_order()
+            if self.cur_ord != order:
+                self.post_order()
         else:
             raise ValueError('Invalid order %s. valid options are {\"pre_order\", \"in_order\", \"post_order\"}'%(order))
 
         for node in self.stack:
             print(node.val, end = ' ')
         print()
+
+    def size(self):
+        if self.root == None:
+            return 0
+        else:
+            self.pre_order()
+            return len(self.stack)
+
+    def __len__(self):
+        return self.size()
+
+    def find(self, val, return_parent = False):
+        if self.root == None:
+            raise ValueError ('Tree is empty!')
+        else:
+            if self.root.val == val:
+                return self.root
+            else:
+                return self.__find(self.root, val, return_parent)
+
+    def __find(self, cur_node, val, return_parent):
+        if val < cur_node.val:
+            if cur_node.left_child != None:
+                if cur_node.left_child.val == val:
+                    if return_parent:
+                        return (cur_node.left_child, cur_node)
+                    return cur_node.left_child
+                else:
+                    return self.__find(cur_node.left_child, val, return_parent)
+            else:
+                return None
+        elif val > cur_node.val:
+            if cur_node.right_child != None:
+                if cur_node.right_child.val == val:
+                    if return_parent:
+                        return (cur_node.right_child, cur_node)
+                    return cur_node.right_child
+                else:
+                    return self.__find(cur_node.right_child, val, return_parent)
+            else:
+                return None
+
+    def copy(self):
+        if self.root == None:
+            raise ValueError ('Tree is empty!')
+        else:
+            self.tmp = Tree()
+            self.tmp.root = Node(self.root.val)
+            self.__copy(self.tmp.root, self.root)
+            tmp = self.tmp
+            del(self.tmp)
+            return tmp
+
+    def __copy(self, cur_trg_node, cur_src_node):
+        if cur_src_node.left_child != None:
+            cur_trg_node.left_child = Node(cur_src_node.left_child.val)
+            self.__copy(cur_trg_node.left_child, cur_src_node.left_child)
+        if cur_src_node.right_child != None:
+            cur_trg_node.right_child = Node(cur_src_node.right_child.val)
+            self.__copy(cur_trg_node.right_child, cur_src_node.right_child)
--- SubhoDataStructure/test_tree.py
+++ SubhoDataStructure/test_tree.py
@@ -3,7 +3,7 @@ from SubhoDataStructure.binary_tree import Tree
 
 class TestTree(TestCase):
     def test_add(self):
-        print('Tesing add method...\n')
+        print('Tesing add method...')
         bin_tree = Tree()
         self.assertEqual(0, bin_tree.add(5))
         self.assertEqual(1, bin_tree.add(3))
@@ -14,7 +14,7 @@ class TestTree(TestCase):
         self.assertEqual(2, bin_tree.add(8))
 
     def test_pre_order(self):
-        print('Tesing pre_order method...\n')
+        print('Tesing pre_order method...')
         bin_tree = Tree()
         for i in [5, 3, 7, 2, 4, 6, 8]:
             bin_tree.add(i)
@@ -24,7 +24,7 @@ class TestTree(TestCase):
             self.assertEqual(tmp_lst[i], j.val)
 
     def test_in_order(self):
-        print('Tesing in_order method...\n')
+        print('Tesing in_order method...')
         bin_tree = Tree()
         for i in [5, 3, 7, 2, 4, 6, 8]:
             bin_tree.add(i)
@@ -34,7 +34,7 @@ class TestTree(TestCase):
             self.assertEqual(tmp_lst[i], j.val)
 
     def test_post_order(self):
-        print('Tesing post_order method...\n')
+        print('Tesing post_order method...')
         bin_tree = Tree()
         for i in [5, 3, 7, 2, 4, 6, 8]:
             bin_tree.add(i)
@@ -44,11 +44,44 @@ class TestTree(TestCase):
             self.assertEqual(tmp_lst[i], j.val)
 
     def test_show(self):
-        print('Tesing show method...\n')
+        print('Tesing show method...')
         bin_tree = Tree()
-        def tmp_func():
-            bin_tree.show(order='any_order')
-        self.assertRaises(ValueError, tmp_func)
+        self.assertRaises(ValueError, lambda : bin_tree.show(order='any_order'))
+
+    def test_size(self):
+        print('Testing size method...')
+        bin_tree = Tree()
+        for i in range(10):
+            bin_tree.add(i)
+        self.assertEqual(10, bin_tree.size())
+
+        bin_tree = Tree()
+        for i in range(5):
+            bin_tree.add(1)
+        self.assertEqual(1, len(bin_tree))
+
+    def test_find(self):
+        print('Testing find method...')
+        bin_tree = Tree()
+        self.assertRaises(ValueError, lambda : bin_tree.find(1))
+        tmp_lst = [5, 3, 2, 4, 7, 6, 8]
+        for i in tmp_lst:
+            bin_tree.add(i)
+        for i in tmp_lst:
+            self.assertEqual(i, bin_tree.find(i).val)
+        self.assertEqual(None, bin_tree.find(50))
+        self.assertEqual(None, bin_tree.find(30))
+
+    def test_copy(self):
+        print('Testing copy method...')
+        bin_tree = Tree()
+        tmp_lst = [5, 3, 2, 4, 7, 6, 8]
+        for i in tmp_lst:
+            bin_tree.add(i)
+        tmp = bin_tree.copy()
+        tmp.pre_order()
+        for i, j in zip(tmp_lst, tmp.stack):
+            self.assertEqual(i, j.val)
 
 if __name__ == '__main__':
     TestTree()
